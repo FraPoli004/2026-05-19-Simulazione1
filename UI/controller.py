deca6aa98e323ab067faa1f75643519791e8f7ac
Francesco Guggia <s325717@studenti.polito.it>
2026-06-28 17:32:07 +0200

    scheletro chinook

--- UI/controller.py
+++ UI/controller.py
@@ -8,60 +8,7 @@ class Controller:
         # the model, which implements the logic of the program and holds the data
         self._model = model
 
-    def fillDDGenre(self):
-        generi = self._model.getGenres()
-        for g in generi:
-            self._view._ddGenre.options.append(ft.dropdown.Option(key=g.GenreId, text=g.Name)
-            )
-        self._view.update_page()
 
     def handleCreaGrafo(self, e):
-        if self._view._ddGenre.value is None:
-            self._view.txt_result.controls.clear()
-            self._view.txt_result.controls.append(ft.Text("Inserisci un genere.", color="red"))
-            self._view.update_page()
-            return
+       pass
 
-        self._model.buildGraph(self._view._ddGenre.value)
-
-        self._view.txt_result.controls.clear()
-        self._view.txt_result.controls.append(ft.Text("Grafo correttamente creato."))
-        self._view.txt_result.controls.append(ft.Text(f"Numero di nodi: {self._model.get_numnodi()}"))
-        self._view.txt_result.controls.append(ft.Text(f"Numero di archi: {self._model.get_numarchi()}"))
-        self.fillDDArtist()
-        best, influenza = self._model.get_artista_piu_influente()
-        self._view.txt_result.controls.append(ft.Text(f"l'artista più influente è: {best} con influenza: {influenza}"))
-        archi5 = self._model.get_top5_archi()
-        self._view.txt_result.controls.append(ft.Text(f"i 5 archi di peso maggiore sono:"))
-        for a in archi5:
-            self._view.txt_result.controls.append(
-                ft.Text(f"{a[0]}------>{a[1]}, peso: {a[2]}"))
-
-
-        self._view.update_page()
-
-    def fillDDArtist(self):
-        artisti = self._model.getNodes()
-        for a in artisti:
-            self._view._ddArtist.options.append(ft.dropdown.Option(key=a.ArtistId, text=a.Name)
-                                               )
-        self._view.update_page()
-
-
-    def handleCammino(self,e):
-        if not self._model.getNodes():
-            self._view.create_alert("Crea prima il grafo!");
-            self._view.update_page()
-            return
-        if self._view._ddArtist.value is None:  # <-- ADATTA al nome del dropdown
-            self._view.create_alert("Seleziona un artista di partenza!")
-            self._view.update_page()
-            return
-
-        partenza = self._model._idMap[int(self._view._ddArtist.value)]
-        cammino, lunghezza = self._model.cammino_peso_crescente(partenza)
-        self._view.txt_result.controls.clear()
-        self._view.txt_result.controls.append(ft.Text(f"il cammino è lungo: {lunghezza}"))
-        for n in cammino:
-            self._view.txt_result.controls.append(ft.Text(f"{n}"))
-        self._view.update_page()
--- database/DAO.py
+++ database/DAO.py
@@ -20,67 +20,6 @@ class DAO():
         conn.close()
         return result
 
-    @staticmethod
-    def getAllArtist(g):
-        conn = DBConnect.get_connection()
-
-        result = []
-
-        cursor = conn.cursor(dictionary=True)
-        query = """select distinct a.ArtistId , a.Name 
-                    from artist a, album al, track t
-                    where a.ArtistId = al.ArtistId and al.AlbumId = t.AlbumId
-                    and t.GenreId = %s"""
-        cursor.execute(query,(g,))
-
-        for row in cursor:
-            result.append(Artista(**row))
-        cursor.close()
-        conn.close()
-        return result
-
-    @staticmethod
-    def getAllEdges(g):
-        conn = DBConnect.get_connection()
-
-        result = []
-
-        cursor = conn.cursor(dictionary=True)
-        query = """select al1.ArtistId as a1 , al2.ArtistId as a2
-                    from album al1, album al2,track t1 , track t2, invoiceline il1, invoiceline il2, invoice i1, invoice i2
-                    where al1.AlbumId = t1.AlbumId and t1.TrackId = il1.TrackId and il1.InvoiceId = i1.InvoiceId 
-                    and al2.AlbumId = t2.AlbumId and t2.TrackId = il2.TrackId and il2.InvoiceId = i2.InvoiceId 
-                    and al1.ArtistId < al2.ArtistId 
-                    and i1.CustomerId = i2.CustomerId
-                    and t1.GenreId = %s and t2.GenreId = %s
-                    group by al1.ArtistId , al2.ArtistId"""
-        cursor.execute(query, (g,g))
-
-        for row in cursor:
-            result.append((row['a1'],row['a2']))
-        cursor.close()
-        conn.close()
-        return result
-    @staticmethod
-    def getAllPop(g):
-        conn = DBConnect.get_connection()
-
-        result = []
-
-        cursor = conn.cursor(dictionary=True)
-        # Popolarità dell'artista calcolata sui brani acquistati del genere selezionato
-        query = """select a.ArtistId as art, count(i.TrackId) as pop
-                    from album a, track t, invoiceline i
-                    where a.AlbumId = t.AlbumId and t.TrackId = i.TrackId
-                    and t.GenreId = %s
-                    group by a.ArtistId"""
-        cursor.execute(query, (g,))
-
-        for row in cursor:
-            result.append((row['art'], row['pop']))
-        cursor.close()
-        conn.close()
-        return result
 
 
 
